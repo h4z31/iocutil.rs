d273dcbc3b74bc61dd42c9d6bef8012ee2a145eb
Hiromitsu Oshiba <0x75960@chocola.tech>
2019-11-08 10:40:21 +0900

    :hammer: remove mutex in hasher

--- src/hasher.rs
+++ src/hasher.rs
@@ -5,21 +5,20 @@ use crypto::md5::Md5;
 use crypto::sha1::Sha1;
 use crypto::sha2::Sha256;
 use std::io::{Error, Write};
-use std::sync::Mutex;
 
 /// Hash calculator
 pub struct Hasher {
-    sha256: Mutex<Sha256>,
-    sha1: Mutex<Sha1>,
-    md5: Mutex<Md5>,
+    sha256: Sha256,
+    sha1: Sha1,
+    md5: Md5,
 }
 
 impl Default for Hasher {
     fn default() -> Self {
         Hasher {
-            sha256: Mutex::new(Sha256::new()),
-            sha1: Mutex::new(Sha1::new()),
-            md5: Mutex::new(Md5::new()),
+            sha256: Sha256::new(),
+            sha1: Sha1::new(),
+            md5: Md5::new(),
         }
     }
 }
@@ -42,26 +41,20 @@ impl Hasher {
     /// std::io::copy(&mut f, &mut hasher).unwrap();
     /// println!("{:?}", hasher.digests());
     /// ```
-    pub fn digests(self) -> ContentHash {
-        let mut sha256 = self.sha256.lock().unwrap();
-        let mut sha1 = self.sha1.lock().unwrap();
-        let mut md5 = self.md5.lock().unwrap();
+    pub fn digests(&mut self) -> ContentHash {
         ContentHash {
-            sha256: SampleHash::new(sha256.result_str()).unwrap(),
-            sha1: SampleHash::new(sha1.result_str()).unwrap(),
-            md5: SampleHash::new(md5.result_str()).unwrap(),
+            sha256: SampleHash::new(self.sha256.result_str()).unwrap(),
+            sha1: SampleHash::new(self.sha1.result_str()).unwrap(),
+            md5: SampleHash::new(self.md5.result_str()).unwrap(),
         }
     }
 }
 
 impl Write for Hasher {
     fn write(&mut self, buf: &[u8]) -> Result<usize, Error> {
-        let mut sha256 = self.sha256.lock().unwrap();
-        let mut sha1 = self.sha1.lock().unwrap();
-        let mut md5 = self.md5.lock().unwrap();
-        sha256.input(buf);
-        sha1.input(buf);
-        md5.input(buf);
+        self.sha256.input(buf);
+        self.sha1.input(buf);
+        self.md5.input(buf);
         Ok(buf.len())
     }
 
